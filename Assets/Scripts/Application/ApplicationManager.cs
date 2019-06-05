a8eac3637653d6b96b5e79067773f899cd18a33b
blind56 <petrinko6@gmail.com>
2019-06-05 20:31:42 +0300

    Added sample code

---
+++ Assets/Scripts/Application/ApplicationManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Application
+{
+	public class ApplicationManager : MonoBehaviour
+	{
+		public event Action ButtonClicked;
+		
+		[SerializeField] private Button someButton;
+		[SerializeField] private Image image;
+		
+		public MainMenuManager MainMenuManager { get; }
+
+		private void Awake()
+		{
+			DontDestroyOnLoad(this);
+			
+			someButton.onClick.AddListener(OnSomeButtonClicked);
+
+			ButtonClicked += OnButtonClicked;
+		}
+
+		private void OnButtonClicked()
+		{
+			// always unsubscribe from the event
+			image.color = randomColor;
+		}
+		
+		private void OnSomeButtonClicked()
+		{
+			ButtonClicked?.Invoke();
+		}
+
+		private void OnDestroy()
+		{
+			ButtonClicked -= OnButtonClicked;
+		}
+	}
+}
